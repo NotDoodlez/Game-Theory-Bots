1ebe4baf26b22ffd40b0615844d3bc604d8b939f
Atharv Dhiman <atharv.emails@gmail.com>
2026-09-06 14:42:42 +0400

    fixed parse error and logic errors in code

--- DelayedTforT.js
+++ DelayedTforT.js
@@ -1,29 +1,28 @@
 export default function bot({ history, memory }) {
     //Its a tit for tat bot for now but I want it to delay its response
 
-    memory = memory ?? { opponentDefected: false }
+    memory = memory ?? { opponentDefected: false, counter: 0 }
 
-    const counter = 0
-    const move = "C"
+    let move = "C"
     const lastOpponentMove = history.at(-1)?.opponent
     if (lastOpponentMove === "D")
         memory.opponentDefected = true
     if (lastOpponentMove === "C")
         memory.opponentDefected = false
 
-    if (memory.opponentDefected === true && counter < 1) 
+    if (memory.opponentDefected === true && memory.counter < 1) 
         move = "C"
-        counter = 1
+       memory.counter = 1
     
-    if (memory.opponentDefected === true && counter >= 1)
+    if (memory.opponentDefected === true && memory.counter >= 1)
         move = "D"
     
-    if (memory.opponentDefected === false && counter < 1)
+    if (memory.opponentDefected === false && memory.counter < 1)
         move =  "C"
     
-    if (memory.opponentDefected === false && counter >= 1)
+    if (memory.opponentDefected === false && memory.counter >= 1)
         move = "D"
-        counter = 0
+        memory.counter = 0
 
 
 
